fb985101d1ec3de483248b69cd3c45da84f6a5b1
Sylvan Franklin <sylvanfranklin@icloud.com>
2024-03-16 09:40:38 -0400

    removed firebase

--- .firebase/bulk-9166d/functions/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.b2c52440.js","_app/immutable/chunks/scheduler.430fa0b3.js","_app/immutable/chunks/index.b9569212.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .firebase/bulk-9166d/functions/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.c7d9b468.js","_app/immutable/chunks/scheduler.430fa0b3.js","_app/immutable/chunks/index.b9569212.js"];
-export const stylesheets = ["_app/immutable/assets/2.31063969.css"];
-export const fonts = [];
